d3f90a7dbee2b990cadd6ed82c29e472f5ec7fff
SungKihun <tjdrlgns3293@gmail.com>
2021-08-17 20:29:22 +0900

    RxSwift 예제

---
+++ CitySearcher/TableViewController.swift
@@ -0,0 +1,102 @@
+//
+//  TableViewController.swift
+//  CitySearcher
+//
+//  Created by 성기훈 on 2021/08/17.
+//
+
+import RxCocoa
+import RxSwift
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var shownCities = [String]()
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"]
+    let disposeBag = DisposeBag() // 뷰가 할당 해제될 때 놓아줄 수 있는 일회용품의 가방
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        
+        searchBar
+            .rx.text // RxCocoa의 Observable 속성
+            .orEmpty // 옵셔널이 아니도록 만듭니다.
+            .debounce(RxTimeInterval.milliseconds(500), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .subscribe(onNext: { [unowned self] query in
+                self.shownCities = self.allCities.filter { $0.hasPrefix(query) }
+                self.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return shownCities.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+
+        cell.textLabel?.text = shownCities[indexPath.row]
+
+        return cell
+    }
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
